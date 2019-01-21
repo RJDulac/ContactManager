9d9fbefc62c50101b8790beed1387dd2e7b0ccb2
Ryan Dulac <analyticrye@gmail.com>
2019-01-21 11:51:09 -0500

    project started

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import Header from "./components/layout/Header";
+import { Provider } from "./context";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Contacts from "./components/contacts/Contacts";
+import AddContact from "./components/contacts/AddContact";
+
+class App extends Component {
+  render() {
+    return (
+      <Provider>
+        <div className="App">
+          <Header branding="Contact Manager" />
+          <div className="container">
+            <AddContact />
+            <Contacts />
+          </div>
+        </div>
+      </Provider>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/contacts/Contacts.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import Contact from "./Contact";
+import { Consumer } from "../../context";
+
+class Contacts extends Component {
+  render() {
+    return (
+      <Consumer>
+        {value => {
+          const { contacts } = value;
+          return (
+            //react fragment takes out unnecessary elements you don't need
+            <React.Fragment>
+              {contacts.map(contact => (
+                <Contact key={contact.id} contact={contact} />
+              ))}
+            </React.Fragment>
+          );
+        }}
+      </Consumer>
+    );
+  }
+}
+
+export default Contacts;
